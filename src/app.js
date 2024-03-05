ef4d069effd7cc0aba316ea82f99d358d1b90b40
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-05 23:50:32 +0900

    app.js 업데이트 및 에러 처리 및 로깅을 위한 미들웨어 추가

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import categoryRouter from "./routes/categories.router.js";
 import menuRouter from "./routes/menus.router.js";
 import UsersRouter from "./routes/users.router.js";
+import LogMiddleware from './middlewares/log.middleware.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(LogMiddleware);
 app.use(express.json());
+
 
 app.use("/api", [categoryRouter, menuRouter, UsersRouter]);
 
---
+++ src/middlewares/log.middleware.js
@@ -0,0 +1,28 @@
+import winston from 'winston';
+
+// 로거 인스턴스 생성
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+// 로그 미들웨어 함수
+export default function (req, res, next) {
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    const start = new Date().getTime();
+
+    res.on('finish', () => {
+      const duration = new Date().getTime() - start;
+      logger.info({
+        Method: req.method,
+        URL: req.url,
+        Status: res.statusCode,
+        Duration: `${duration}ms`
+      });
+    });
+  }
+  next();
+};
