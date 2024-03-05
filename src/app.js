90e9b3a837352e752ace38511d8cff326d61dc03
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-05 22:21:04 +0900

    ㅎㅎ

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from "express";
 import categoryRouter from "./routes/categories.router.js";
 import menuRouter from "./routes/menus.router.js";
-import signupRouter from "./routes/signup.router.js";
-import loginRouter from "./routes/login.router.js";
+import UsersRouter from "./routes/users.router.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/api", [categoryRouter, menuRouter, signupRouter, loginRouter]);
+app.use("/api", [categoryRouter, menuRouter, UsersRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,9 +2,29 @@ import jwt from 'jsonwebtoken';
 import { prisma } from "../utils/prisma/index.js";
 
 export default async function(req, res, next) {
-    const {authorization} = req.cookies;
-    if (!authorization) throw new Error('요청한 사용자의 토큰이 존재하지 않습니다.');
+    try{
+        const {authorization} = req.cookies;
+        if (!authorization) throw new Error('요청한 사용자의 토큰이 존재하지 않습니다.');
 
-    const [tokenType, token] = authorization.split(' ');
-    if(tokenType !== 'bearer') throw new Error('요청한 사용자의 토큰이 존재하지 않습니다.');
+        const [tokenType, token] = authorization.split(' ');
+        if(tokenType !== 'bearer') throw new Error('토큰타입이 아닙니다.');
+
+        const decodesToken = jwt.verify(token, 'custom-secret-key');
+        const userId = decodesToken.userId;
+
+        const user = await prisma.users.findFirst({
+            where: {userId: +userId}
+        });
+
+        if(!user) throw new Error('토큰 사용자가 존재하지 않습니다.')
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        if(error.name === 'TokenExpiredError')
+        return res.status(401).json({  message: '토큰이 만료되었습니다.'})
+
+        return res.status(400).json({ message: error.message})
+    }
 }
