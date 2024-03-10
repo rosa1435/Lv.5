96d738398389aa0a1e13334bdfddab0291f24f50
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-10 11:58:14 +0900

    짜증나네

--- src/middlewares/log.middleware.js
+++ src/middlewares/log.middleware.js
@@ -1,28 +1,26 @@
-import winston from 'winston';
+import winston from "winston";
 
 // 로거 인스턴스 생성
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   format: winston.format.json(),
-  transports: [
-    new winston.transports.Console(),
-  ],
+  transports: [new winston.transports.Console()],
 });
 
 // 로그 미들웨어 함수
 export default function (req, res, next) {
-  if (req.method !== 'GET' && req.method !== 'HEAD') {
+  if (req.method !== "GET" && req.method !== "HEAD") {
     const start = new Date().getTime();
 
-    res.on('finish', () => {
+    res.on("finish", () => {
       const duration = new Date().getTime() - start;
       logger.info({
         Method: req.method,
         URL: req.url,
         Status: res.statusCode,
-        Duration: `${duration}ms`
+        Duration: `${duration}ms`,
       });
     });
   }
   next();
-};
+}
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,98 +1,109 @@
 import express from "express";
 import { prisma } from "../utils/prisma/index.js";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import joi from "joi";
 
 const router = express.Router();
 
+const createUser = joi.object({
+
+});
+
 // 회원가입 API
-router.post('/sign-up', async (req, res, next) => {
-    const {nickname, password, userType} = req.body
-    try {
-      if (!nickname || !password) { // 데이터형식의 오류
-        const err = new Error('데이터 형식이 올바르지 않습니다.');
-        err.status = 400;
-        throw err;
-      }
-    
+router.post("/sign-up", async (req, res, next) => {
+  const { nickname, password, userType } = req.body;
+  try {
+    if (!nickname || !password) {
+      // 데이터형식의 오류
+      const err = new Error("데이터 형식이 올바르지 않습니다.");
+      err.status = 400;
+      throw err;
+    }
 
-        if (!/^[a-zA-Z0-9]{3,15}$/.test(nickname)) { // 닉네임 형식 오류
-          const err = new Error('닉네임 형식에 일치하지 않습니다.');
-          err.status = 400;
-          throw err;
-        }
+    if (!/^[a-zA-Z0-9]{3,15}$/.test(nickname)) {
+      // 닉네임 형식 오류
+      const err = new Error("닉네임 형식에 일치하지 않습니다.");
+      err.status = 400;
+      throw err;
+    }
 
-        if (password.length < 8 || password.length > 20 || password.includes(nickname)) { // 비밀번호 형식 오류
-          const err = new Error('비밀번호 형식에 일치하지 않습니다.');
-          err.status = 400;
-          throw err;
-        }
-      
-        const findNickname = await prisma.users.findFirst({
-          where: {
-            nickname: nickname
-          }
-    })
+    if (
+      password.length < 8 ||
+      password.length > 20 ||
+      password.includes(nickname)
+    ) {
+      // 비밀번호 형식 오류
+      const err = new Error("비밀번호 형식에 일치하지 않습니다.");
+      err.status = 400;
+      throw err;
+    }
 
-    if (findNickname) {  // 중복된 닉네임 오류
-      const err = new Error('중복된 닉네임입니다.');
+    const findNickname = await prisma.users.findFirst({
+      where: {
+        nickname: nickname,
+      },
+    });
+
+    if (findNickname) {
+      // 중복된 닉네임 오류
+      const err = new Error("중복된 닉네임입니다.");
       err.status = 409;
       throw err;
     }
-  
-        const hashedPassword = await bcrypt.hash(password,10);
 
-        const user = await prisma.users.create({
-            data: {
-                nickname,
-                password: hashedPassword,
-                userType,
-            }
-        })
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        return res.status(201).json({message: "회원가입이 완료되었습니다."})
-      } catch (err) {
-        next(err);
-      }
-    })
-    
+    const user = await prisma.users.create({
+      data: {
+        nickname,
+        password: hashedPassword,
+        userType,
+      },
+    });
+
+    return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+  } catch (err) {
+    next(err);
+  }
+});
 
 // 로그인 API
-router.post('/sign-in', async (req, res, next) => {
-    const {nickname, password} = req.body
-    try {
-      if (!nickname || !password) { // 데이터형식의 오류
-        const err = new Error('데이터 형식이 올바르지 않습니다.');
-        err.status = 400;
-        throw err;
-      }
-      const user = await prisma.users.findFirst({where: {nickname}});
+router.post("/sign-in", async (req, res, next) => {
+  const { nickname, password } = req.body;
+  try {
+    if (!nickname || !password) {
+      // 데이터형식의 오류
+      const err = new Error("데이터 형식이 올바르지 않습니다.");
+      err.status = 400;
+      throw err;
+    }
+    const user = await prisma.users.findFirst({ where: { nickname } });
 
-      if (!user) {  // 존재하지 않는 닉네임
-        const err = new Error('존재하지 않는 닉네임 입니다.');
-        err.status = 401;
-        throw err;
-      }
+    if (!user) {
+      // 존재하지 않는 닉네임
+      const err = new Error("존재하지 않는 닉네임 입니다.");
+      err.status = 401;
+      throw err;
+    }
 
-      const userPassword = await bcrypt.compare(password, user.password);
+    const userPassword = await bcrypt.compare(password, user.password);
 
-      if (!userPassword) {  // 존재하지 않는 패스원드
-        const err = new Error('비밀번호가 일치하지 않습니다.');
-        err.status = 401;
-        throw err;
-      }
+    if (!userPassword) {
+      // 존재하지 않는 패스원드
+      const err = new Error("비밀번호가 일치하지 않습니다.");
+      err.status = 401;
+      throw err;
+    }
 
-        const token = jwt.sign(
-            { userId: +user.id},
-            'custom-secret-key'
-        );
+    const token = jwt.sign({ userId: +user.id }, "custom-secret-key");
 
-            res.cookie('authorization', `Bearer ${token}`);
-            return res.status(201).json({message: "로그인에 성공하였습니다."})
+    res.cookie("authorization", `Bearer ${token}`);
+    return res.status(201).json({ message: "로그인에 성공하였습니다." });
+  } catch (err) {
+    next(err);
+  }
+});
 
-      } catch (err) {
-        next(err);
-      }
-    });
-    
+
 export default router;
